1b48ce760a87f2f14c254392e4c40d7fcf80c260
tincogs <tomoday91@gmail.com>
2020-10-27 11:27:20 -0500

    commit tree f5dfc65051622992516ee177e3ebb2bf47beef1a

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  const timeNum = parseInt(time)
+  if (timeNum < 12) return "Good Morning"
+  if (timeNum > 17) return "Good Evening"
+  return "Good Afternoon"
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(greeting) {
+  document.getElementById("greeting").innerText = greeting;
+}
